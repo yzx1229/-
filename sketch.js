96b9511bd6963a17cadba37ba01b7351610bc82e
yzx1229 <413730119@o365.tku.edu.tw>
2025-04-25 11:00:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let circles = [];
+let hearts = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // 產生 40 個圓
+  // 產生 40 個愛心
   for (let i = 0; i < 40; i++) {
-    circles.push({
+    hearts.push({
       x: random(width),
       y: random(height),
       size: random(30, 100),
@@ -19,10 +19,19 @@ function draw() {
   // 計算滑鼠影響的大小
   let sizeFactor = map(mouseX, 0, width, 10, 120);
 
-  // 繪製圓
-  for (let circle of circles) {
-    fill(circle.color);
+  // 繪製愛心
+  for (let heart of hearts) {
+    fill(heart.color);
     noStroke();
-    ellipse(circle.x, circle.y, circle.size * (sizeFactor / 100));
+    drawHeart(heart.x, heart.y, heart.size * (sizeFactor / 100));
   }
+}
+
+// 繪製愛心的函式
+function drawHeart(x, y, size) {
+  beginShape();
+  vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
 }
